7e5db35d93bda352284d9b92bf8e63e7078da535
tomo <tmrtkm1227tksx@gmail.com>
2021-11-01 00:15:43 +0900

    [add] Simply flask code

---
+++ web_and_internet/flask_test.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import g
+from flask import render_template
+from flask import request
+from flask import Response
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+def main():
+    app.debug = True
+    app.run(host="127.0.0.1", port=5000)
+
+
+if __name__ == "__main__":
+    main()
