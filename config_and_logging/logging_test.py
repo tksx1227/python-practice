8425b6265cc654a209204c981a14c23b3bfee422
tomo <tmrtkm1227tksx@gmail.com>
2021-11-09 23:05:46 +0900

    [update] Add formatter

--- config_and_logging/logging_test.py
+++ config_and_logging/logging_test.py
@@ -12,8 +12,13 @@ INFO
 DEBUG
 """
 
-# filename を指定すると、ログをファイルに書き出すことができる
-logging.basicConfig(filename="test.log", level=logging.DEBUG)
+# # filename を指定すると、ログをファイルに書き出すことができる
+# logging.basicConfig(filename="test.log", level=logging.DEBUG)
+
+# formatter を使うとログメッセージをカスタマイズできる
+# formatter = "%(levelname)s:%(message)s"
+formatter = "%(asctime)s:%(message)s"
+logging.basicConfig(level=logging.DEBUG, format=formatter)
 
 logging.critical("critical")
 logging.error("error")
