0685af8472e50de12a09d6ff7dea961f918f8fec
tomo <tmrtkm1227tksx@gmail.com>
2021-11-10 09:06:42 +0900

    [update] Add logging handler

--- config_and_logging/logger_test.py
+++ config_and_logging/logger_test.py
@@ -5,6 +5,10 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# ハンドラーを作成することで、ロガーのメッセージを任意の場所に書き出せる
+h = logging.FileHandler("logtest.log")
+logger.addHandler(h)
+
 
 def do_something():
     logger.info("from logger_test")
