d543591f521dc23436d989d2e1761a744e54c6d4
tomo <tmrtkm1227tksx@gmail.com>
2021-11-10 09:57:09 +0900

    [fix] Changed string to integer

--- config_and_logging/config_test.py
+++ config_and_logging/config_test.py
@@ -7,11 +7,11 @@ config["DEFAULT"] = {
 }
 config["web_server"] = {
     "host": "127.0.0.1",
-    "port": "80"
+    "port": 80
 }
 config["db_server"] = {
     "host": "127.0.0.1",
-    "port": "3306"
+    "port": 3306
 }
 with open("config.ini", "w") as config_file:
     config.write(config_file)
